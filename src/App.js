6fb92bd72e77811d016a5ded572bab1c103794f8
Melyn Atieno Were <melynatieno@gmail.com>
2022-09-20 18:35:58 +0300

    Add files

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Form from './Components/Form';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1><b>TO-DO LIST</b></h1>
+        <h2><b>Plan your day!</b></h2>
+      </header>
+      <Form/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Form.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Form(){
+    return(
+        <div className="input">
+            <form>
+                <label><b>Task 1:</b></label>
+                <input></input>
+                <label><b>Task 2:</b></label>
+                <input></input>
+                <label><b>Task 3:</b></label>
+                <input></input>
+                <label><b>Task 4:</b></label>
+                <input></input>
+                <label><b>Task 5:</b></label>
+                <input></input>
+            </form>
+        </div>
+    )
+}
+
+export default Form;
