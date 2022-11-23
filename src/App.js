785c30855ca19f637946eb0392012620f57610ba
Melyn Atieno Were <melynatieno@gmail.com>
2022-11-23 11:30:26 +0300

    delete files

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import './App.css';
-import Form from './Components/Form';
+
 import React from 'react';
 
 function App() {
   
 return (
     <div className="App">
-      <header className="App-header">
-        <h1><b>TO-DO LIST</b></h1>
-        <h2><b>Plan your day!</b></h2>
-      </header>
-      <Form/>
-      <div className='content'>
-
-      </div>
+      
     </div>
   );
 }
